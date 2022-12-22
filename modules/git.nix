47ad3949b3e422c86885b8883b61a29db9292249
Vlad Petrov <ejiek@pm.me>
2022-12-22 16:14:54 +0300

    [git] fix <git lg> rendering

--- modules/git.nix
+++ modules/git.nix
@@ -19,7 +19,7 @@
     userEmail = "ejiek@pm.me";
     userName = "Vlad Petrov";
     aliases = {
-      lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n''	%C(white)%s%C(reset) %C(dim green)- %an%C(reset)' --all";
+      lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n%C(white)%s%C(reset) %C(dim green)- %an%C(reset)' --all";
     };
     extraConfig = {
       init = {
