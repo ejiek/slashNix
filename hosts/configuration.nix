c536b3e5b3b3f8dd35bace0b1a7ffb0ed260587f
Vlad Petrov <ejiek@pm.me>
2023-01-19 10:40:10 +0300

    enable gpg

--- hosts/configuration.nix
+++ hosts/configuration.nix
@@ -114,10 +114,11 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+   programs.gnupg.agent = {
+     enable = true;
+     #enableSSHSupport = true;
+     pinentryFlavor = "curses";
+   };
 
   # List services that you want to enable:
 
