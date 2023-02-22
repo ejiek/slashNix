793e7de37c0ee1a1626d014d2c1b0de27af809a8
Vlad Petrov <ejiek@pm.me>
2023-02-22 17:45:01 +0800

    [git] add coc & a couple more plugins

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -15,6 +15,10 @@
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
+    extraConfig = ''
+      set rnu
+      set nu
+    '';
     plugins = with pkgs.vimPlugins; [
       {
         plugin = airline;
@@ -33,7 +37,44 @@
       }
       vim-nix
       yankring
+      {
+        plugin = telescope-nvim;
+        config = ''
+          let g:telescope_previewer = 'bat'
+          let mapleader = "\<Space>"
+          nnoremap <leader>ff <cmd>Telescope find_files<cr>
+          nnoremap <leader>fg <cmd>Telescope live_grep<cr>
+          nnoremap <leader>fb <cmd>Telescope buffers<cr>
+          nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+        '';
+      }
+      nvim-treesitter
+      vim-devicons
+      vim-fugitive
+      # vim-rhubarb
+      vim-signify
+      vim-startify
     ];
-
+    coc.enable = true;
   };
+
+  home.packages = with pkgs; [
+    # telescope dependencies
+    bat
+    fd
+    fzf
+    ripgrep
+    # TODO: get rid of node_Packages for every line
+    nodePackages.coc-css
+    nodePackages.coc-eslint
+    nodePackages.coc-html
+    nodePackages.coc-json
+    nodePackages.coc-prettier
+    nodePackages.coc-python
+    nodePackages.coc-rust-analyzer
+    nodePackages.coc-sh
+    nodePackages.coc-tsserver
+    nodePackages.coc-vimlsp
+    nodePackages.coc-yaml
+  ];
 }
