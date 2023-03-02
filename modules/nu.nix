9542a357873e0d8ff0197a9536cd04ec32ca850d
Vlad Petrov <ejiek@pm.me>
2023-03-02 13:39:58 +0800

    [nu] add nixos-rebuild aliases

--- modules/nu.nix
+++ modules/nu.nix
@@ -7,6 +7,10 @@
 {
   programs.nushell = {
     enable = true;
+    extraConfig = ''
+      alias nwitch = sudo nixos-rebuild switch --flake '/home/ejiek/.slashNix/flake.nix#e220'
+      alias ntest = sudo nixos-rebuild test --flake '/home/ejiek/.slashNix/flake.nix#e220'
+    '';
   };
 }
 
