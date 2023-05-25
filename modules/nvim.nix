9f5a864e41b7acd4f8919b7e39a3d73e4afb74dc
Vlad Petrov <ejiek@pm.me>
2023-05-25 22:20:15 +0300

    [nvim] ThePrimeagen basics

--- modules/nvim.nix
+++ modules/nvim.nix
@@ -15,6 +15,7 @@
     viAlias = true;
     vimAlias = true;
     vimdiffAlias = true;
+    defaultEditor = true;
     extraConfig = ''
       " Show current line and distance from it
       set rnu
@@ -30,7 +31,11 @@
       set list
       set listchars=tab:▸\ ,trail:·
 
+      " migh not be needed with vimlastplace
       set viminfo='10,\"100,:20,%,n~/.viminfo
+
+      " Fixes file creation in netrw
+      set shell=/etc/profiles/per-user/ejiek/bin/zsh
 
       " Better window navigation
       nnoremap <C-h> <C-w>h
@@ -38,6 +43,8 @@
       nnoremap <C-k> <C-w>k
       nnoremap <C-l> <C-w>l
       " Split vetically with ctrl+| and horizontally with ctrl+/
+
+      nnoremap <leader>pv :Explore<cr>
     '';
     plugins = with pkgs.vimPlugins; [
       {
@@ -61,19 +68,83 @@
         plugin = telescope-nvim;
         config = ''
           let g:telescope_previewer = 'bat'
+          " mapleader defined here because extraConfig is evaluated after plugins
           let mapleader = "\<Space>"
           nnoremap <leader>ff <cmd>Telescope find_files<cr>
-          nnoremap <leader>fg <cmd>Telescope live_grep<cr>
-          nnoremap <leader>fb <cmd>Telescope buffers<cr>
-          nnoremap <leader>fh <cmd>Telescope help_tags<cr>
+          nnoremap <leader>fg <cmd>Telescope git_files<cr>
+          nnoremap <leader>fl <cmd>Telescope live_grep<cr>
         '';
       }
-      nvim-treesitter
+      nvim-cmp
+      {
+        plugin = nvim-treesitter;
+        type = "lua";
+        config = ''
+          require'nvim-treesitter.configs'.setup {
+            highlight = {
+              enable = true,
+            },
+            additional_vim_regex_highlighting = false,
+          }
+        '';
+      }
+      nvim-treesitter-parsers.bash
+      nvim-treesitter-parsers.css
+      nvim-treesitter-parsers.dockerfile
+      nvim-treesitter-parsers.git_config
+      nvim-treesitter-parsers.git_rebase
+      nvim-treesitter-parsers.gitcommit
+      nvim-treesitter-parsers.gitignore
+      nvim-treesitter-parsers.go
+      nvim-treesitter-parsers.html
+      nvim-treesitter-parsers.javascript
+      nvim-treesitter-parsers.json
+      nvim-treesitter-parsers.lua
+      nvim-treesitter-parsers.markdown
+      nvim-treesitter-parsers.nix
+      nvim-treesitter-parsers.python
+      nvim-treesitter-parsers.rust
+      nvim-treesitter-parsers.sql
+      nvim-treesitter-parsers.terraform
+      nvim-treesitter-parsers.latex
+      nvim-treesitter-parsers.toml
+      nvim-treesitter-parsers.typescript
+      nvim-treesitter-parsers.yaml
+      {
+        plugin = harpoon;
+        type = "lua";
+        config = ''
+          local mark = require("harpoon.mark")
+          local ui = require("harpoon.ui")
+
+          vim.keymap.set("n", "<leader>a", mark.add_file)
+          vim.keymap.set("n", "<C-d>", ui.toggle_quick_menu)
+
+          vim.keymap.set("n", "<C-j>", function() ui.nav_file(1) end)
+          vim.keymap.set("n", "<C-k>", function() ui.nav_file(2) end)
+          vim.keymap.set("n", "<C-l>", function() ui.nav_file(3) end)
+          vim.keymap.set("n", "<C-Semicolon>", function() ui.nav_file(4) end)
+        '';
+      }
       vim-devicons
-      vim-fugitive
+      {
+        plugin = vim-fugitive;
+        type = "lua";
+        config = ''
+          vim.keymap.set("n", "<leader>gs", ":Git<CR>")
+          vim.keymap.set("n", "<leader>gb", ":Git blame<CR>")
+        '';
+      }
       # vim-rhubarb
       vim-signify
-      vim-startify
+      vim-lastplace
+      {
+        plugin = undotree;
+        type = "lua";
+        config = ''
+          vim.keymap.set("n", "<leader>u", ":UndotreeToggle<CR>")
+        '';
+      }
     ];
   };
 
