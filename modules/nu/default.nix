113d632802f144941343bc8b4e4bbc0ca35a5f23
Vlad Petrov <ejiek@pm.me>
2023-08-30 15:38:05 +0300

    chore: formatting

--- modules/nu/default.nix
+++ modules/nu/default.nix
@@ -14,8 +14,8 @@ in {
       programs.nushell = {
         enable = true;
         extraConfig = ''
-          alias nwitch = sudo nixos-rebuild switch --flake '/home/ejiek/.slashNix/flake.nix#e220'
-          alias ntest = sudo nixos-rebuild test --flake '/home/ejiek/.slashNix/flake.nix#e220'
+        alias nwitch = sudo nixos-rebuild switch --flake '/home/ejiek/.slashNix/flake.nix#e220'
+        alias ntest = sudo nixos-rebuild test --flake '/home/ejiek/.slashNix/flake.nix#e220'
         '';
       };
 
