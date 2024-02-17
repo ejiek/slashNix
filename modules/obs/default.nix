dbebd4b625a91a19e44bf60c43632cc1e54e0652
Vlad Petrov <git@ejiek.id>
2024-02-18 01:15:43 +0300

    [obs] sort & add obs-shaderfilter

--- modules/obs/default.nix
+++ modules/obs/default.nix
@@ -22,8 +22,9 @@ in {
       programs.obs-studio = {
         enable = true;
         plugins = with pkgs.obs-studio-plugins; [
-          wlrobs
           obs-pipewire-audio-capture
+          obs-shaderfilter
+          wlrobs
         ];
       };
     };
