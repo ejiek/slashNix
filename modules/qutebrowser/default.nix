fafb0d7283df63aebfe4c83aaed3210096c05428
Vlad Petrov <git@ejiek.id>
2024-03-25 11:32:40 +0300

    [qute] add nix pr traker as a search engine

--- modules/qutebrowser/default.nix
+++ modules/qutebrowser/default.nix
@@ -63,6 +63,7 @@ in {
           mynix = "https://mynixos.com/search?q={}";
           netflix = "https://www.netflix.com/search?q={}";
           nix = "https://search.nixos.org/packages?channel=unstable&from=0&size=50&sort=relevance&type=packages&query={}";
+          npr = "https://nixpk.gs/pr-tracker.html?pr={}";
           nw = "https://nixos.wiki/index.php?search={}&go=Go";
           pac = "https://www.archlinux.org/packages/?q={}";
           paca = "https://www.archlinuxarm.org/packages/?q={}";
